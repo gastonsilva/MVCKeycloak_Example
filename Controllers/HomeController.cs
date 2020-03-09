7e1d3a17a2f9e68ae38f71bb6cafb20f500ef1c9
Gastón Silva <gaston.silva@gmail.com>
2020-03-09 01:01:22 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+
+namespace MVCKeycloak_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var userPrinciple = User as ClaimsPrincipal;
+
+            return View(userPrinciple);
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Owin.Security.Keycloak;
+using System;
+using System.Configuration;
+
+[assembly: OwinStartup(typeof(MVCKeycloak_Example.Startup))]
+
+namespace MVCKeycloak_Example
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            const string persistentAuthType = "KeycloakOwinAuthenticationSample_cookie_auth";
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = persistentAuthType
+            });
+            app.SetDefaultSignInAsAuthenticationType(persistentAuthType);
+
+            app.UseKeycloakAuthentication(new KeycloakAuthenticationOptions
+            {
+                ClientId = ConfigurationManager.AppSettings["keycloak-clientid"],
+                ClientSecret = ConfigurationManager.AppSettings["keycloak-clientsecret"],
+                Realm = ConfigurationManager.AppSettings["keycloak-realm"],
+                KeycloakUrl = ConfigurationManager.AppSettings["keycloak-url"],
+
+                VirtualDirectory = "",
+                SignInAsAuthenticationType = persistentAuthType,
+                AuthenticationType = persistentAuthType,
+                DisableAllRefreshTokenValidation = true,
+                DisableAudienceValidation = true,
+                EnableBearerTokenAuth = true,
+                TokenClockSkew = TimeSpan.FromSeconds(2)
+            });
+        }
+    }
+}
