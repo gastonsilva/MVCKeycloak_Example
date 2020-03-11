1c17d1df36593b94e7b371925a8e3e8e4ceb6acc
Gastón Silva <gaston.silva@gmail.com>
2020-03-11 15:45:11 -0300

    Fix assemblyinfo Fix references

--- Keycloak.IdentityModel/Properties/AssemblyInfo.cs
+++ Keycloak.IdentityModel/Properties/AssemblyInfo.cs
@@ -1,5 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("Microsoft.Owin 3.1 Authentication Middleware for JBoss Keycloak 3.2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.5.0")]
 
 [assembly: AssemblyTitle("Keycloak.IdentityModel")]
 [assembly: Guid("fa6dddd7-366a-4012-acf2-252c599f80c0")]
--- Owin.Security.Keycloak/Properties/AssemblyInfo.cs
+++ Owin.Security.Keycloak/Properties/AssemblyInfo.cs
@@ -1,5 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("Microsoft.Owin 3.1 Authentication Middleware for JBoss Keycloak 3.2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.5.0")]
 
 [assembly: AssemblyTitle("Owin.Security.Keycloak")]
 [assembly: Guid("fcac3105-3e81-49c4-9d8e-0fc9e608e87c")]
